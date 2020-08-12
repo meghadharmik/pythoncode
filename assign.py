220e26ea8fa3b844e7e2ea36c59f943197a0ca83
meghadharmik <69583429+meghadharmik@users.noreply.github.com>
2020-08-12 07:42:03 -0700

    To perform data structure
    assigning, accessing and deleting different elements of data structure

---
+++ assign.py
@@ -0,0 +1,24 @@
+#assigning elements to different lists
+mylist1 = ["tomato","potato","carrot"]
+mylist2 = ["coriandar","spinach","fenugreek"]
+
+print (mylist1)
+print (mylist2)
+
+
+
+#accessing element from a tuple
+mytuple = ("11","56","34","23","1","50")
+
+print (mytuple[4])
+
+
+
+#deleting different dictionary elements 
+dict = {'Name': 'Minal',
+        'Age': 7,
+        'Class': 'First'
+       }
+del dict['Age']; 
+           
+print (dict)
